3523135ebd17e7678f1a9f97aecd6931d2882b98
Megan Walter <megwalter@hotmail.com>
2016-03-06 16:06:20 -0500

    working on article View js

--- class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
@@ -30,29 +30,27 @@ articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     if ($(this).val()) {
       $('article').hide();
-      $('article').filter('article[data-author ="' + $(this).val() +'"]').fadeIn(500);
-
+      $('article').filter('article[data-author ="' + $(this).val() + '"]').fadeIn(500);
     } else {
       $('article').filter(!'article[class="template"]').fadeIn(500);
     }
+
     $('#category-filter').val('');
   });
 };
 
 articleView.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function(){
+  $('#category-filter').on('change', function() {
     if ($(this).val()) {
       $('article').hide();
-      $('article').filter('article[data-category="' + $(this).val() +'"]').fadeIn(500);
+      $('article').filter('article[data-category="' + $(this).val() + '"]').fadeIn(500);
     } else {
-      $('artcile').filter(!'article[class="template"]').fadeIn(500)
+      $('article').filter(!'article[class="template"]').fadeIn(500);
     }
+
     $('#author-filter').val('');
-  })
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
-  //       When an option with a value is selected, hide all the articles, then reveal the matches.
-  //       When the blank (default) option is selected, show all the articles, except for the template.
-  //       Be sure to reset the #author-filter while you are at it!
+
+  });
 
 };
 
@@ -62,9 +60,17 @@ articleView.handleMainNav = function() {
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
   //       data available to you on the .tab element that was clicked.
-  $('.main-nav').on(/* CODE GOES HERE */);
+  $('.main-nav').on('click', 'li', function() {
+    if ($(this).attr('data-content') === 'articles') {
+      $('.tab-content').hide();
+      $('article').filter(!'.template').fadeIn(500);
+    } else if ($(this).attr('data-content') === 'about') {
+      $('.tab-content').hide();
+      $('#about').fadeIn(500);
+    }
+  }/* CODE GOES HERE */);
 
-  $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
+  // $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
 
 articleView.setTeasers = function() {
@@ -83,4 +89,5 @@ $(document).ready(function(){
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
+  articleView.handleMainNav();
 })
