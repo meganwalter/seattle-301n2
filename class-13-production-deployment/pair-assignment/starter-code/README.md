3daa5b684687e77ee4c8db114d13e7e96637fe46
Megan Walter <megwalter@hotmail.com>
2016-04-06 23:07:05 -0400

    We copied the code over from our heroku repo so that we could turn it in. The working heroku app is on our README

---
+++ class-13-production-deployment/pair-assignment/starter-code/README.md
@@ -0,0 +1,5 @@
+# Class 13 = success!
+
+See our app below:
+
+[Our Ancient Plains Heroku App](https://ancient-plains-67797.herokuapp.com/about)
--- class-13-production-deployment/pair-assignment/starter-code/public/scripts/repo.js
+++ class-13-production-deployment/pair-assignment/starter-code/public/scripts/repo.js
@@ -3,20 +3,19 @@
 
   repos.all = [];
 
-  // TODO: Refactor and simplify this ajax call into a get request
+  // DONE: Refactor and simplify this ajax call into a get request
   // to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/brookr/repos' +
-            '?per_page=100' +
-            '&sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
-      success: function(data, message, xhr) {
-        repos.all = data;
-      }
-    }).done(callback);
-  };
+      $.ajax({
+        url: '/github/user/repos' +
+              '?per_page=100' +
+              '&sort=updated',
+        type: 'GET',
+        success: function(data, message, xhr) {
+          repos.all = data;
+        }
+      }).done(callback);
+    };
 
   repos.with = function(attr) {
     return repos.all.filter(function(repo) {
